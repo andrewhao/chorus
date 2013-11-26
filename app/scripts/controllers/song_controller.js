091de897abf8e619de72fc8338968a6b1331765e
Andrew Hao <ahao@blurb.com>
2013-11-25 19:33:41 -0800

    Fast styles + localstorage adapter...

--- app/scripts/controllers/song_controller.js
+++ app/scripts/controllers/song_controller.js
@@ -2,6 +2,13 @@ Hendrix.SongController = Ember.ObjectController.extend({
   actions: {
     edit: function() {
       this.transitionToRoute('song.edit');
+    },
+    delete: function() {
+      // this tells Ember-Data to delete the current user
+      this.get('model').deleteRecord();
+      this.get('model').save();
+      // then transition to the users route
+      this.transitionToRoute('songs');
     }
   }
 });
--- app/scripts/store.js
+++ app/scripts/store.js
@@ -1,2 +1,5 @@
 Hendrix.Store = DS.Store.extend();
-Hendrix.ApplicationAdapter = DS.FixtureAdapter;
+//Hendrix.ApplicationAdapter = DS.FixtureAdapter;
+Hendrix.ApplicationAdapter = DS.LSAdapter.extend({
+   namespace: 'hendrix'
+});
