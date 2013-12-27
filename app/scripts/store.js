305cfdc4ee35686ff36ec8d1a83c623aa003723f
Andrew Hao <andrewhao@gmail.com>
2013-12-27 03:24:49 -0800

    Adding DS.RESTAdapter

--- app/scripts/store.js
+++ app/scripts/store.js
@@ -1,4 +1,7 @@
 Hendrix.Store = DS.Store.extend();
+//Hendrix.ApplicationAdapter = DS.RESTAdapter.extend({
+//  host: "http://localhost:3000"
+//})
 Hendrix.ApplicationAdapter = DS.FixtureAdapter;
 // Hendrix.ApplicationAdapter = DS.LSAdapter.extend({
 //   namespace: 'hendrix'
