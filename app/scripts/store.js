f6f406079250dfd33f593b1bc299e1289dcbb951
Andrew Hao <andrewhao@gmail.com>
2013-12-27 16:50:44 -0800

    Updating RESTAdapter to work with Rails JSON, and CORS

--- app/scripts/store.js
+++ app/scripts/store.js
@@ -1,8 +1,18 @@
+$.ajaxSetup({
+  crossDomain: true,
+  xhrFields: {
+    withCredentials: true
+  }
+});
 Hendrix.Store = DS.Store.extend();
-//Hendrix.ApplicationAdapter = DS.RESTAdapter.extend({
-//  host: "http://localhost:3000"
-//})
-Hendrix.ApplicationAdapter = DS.FixtureAdapter;
+Hendrix.ApplicationAdapter = DS.RESTAdapter.extend({
+  host: "http://localhost:3000",
+  // Force ember-data to append the `json` suffix
+  buildURL: function(record, suffix) {
+    return this._super(record, suffix) + ".json";
+  }
+})
+//Hendrix.ApplicationAdapter = DS.FixtureAdapter;
 // Hendrix.ApplicationAdapter = DS.LSAdapter.extend({
 //   namespace: 'hendrix'
 // });
